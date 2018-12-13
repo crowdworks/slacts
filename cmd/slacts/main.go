7a0086b631fefc48eeec18d96e7ef863b48d1d52
Shota SAWADA <xiootas@gmail.com>
2018-12-13 13:50:58 +0900

    refactor: make cobra command function

--- cmd/slacts/main.go
+++ cmd/slacts/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -19,8 +20,7 @@ var (
 func init() {
 	slackConfig = config.NewSlackClientConfig()
 
-	slackCmd.AddCommand(slackCountCmd)
-	rootCmd.AddCommand(taskCmd, slackCmd)
+	rootCmd.AddCommand(newTaskCmd(), newSlackCmd())
 }
 
 var rootCmd = &cobra.Command{
@@ -28,94 +28,119 @@ var rootCmd = &cobra.Command{
 	Short: "a CLI tool for Slack statistics",
 }
 
-var taskCmd = &cobra.Command{
-	Use:     "task",
-	Short:   "exec tasks",
-	Args:    cobra.ExactArgs(1),
-	Example: "slacts task slacts.yml",
-	Run: func(cmd *cobra.Command, args []string) {
-		tasks, err := config.ReadYaml(args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
+func newTaskCmd() *cobra.Command {
+	type option struct {
+		file string
+	}
 
-		for _, task := range *tasks {
-			switch task.Kind {
-			case "count":
-				query := slacts.NewSlackQuery(task.Query)
+	var opt option
+	cmd := &cobra.Command{
+		Use:     "task",
+		Short:   "exec tasks",
+		Example: "slacts task slacts.yml",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if opt.file == "" {
+				return errors.New("task definition file is required")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			tasks, err := config.ReadYaml(opt.file)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-				if slackConfig.Token() == "" {
-					log.Fatalln("unset Slack API Token")
-				}
+			for _, task := range *tasks {
+				switch task.Kind {
+				case "count":
+					query := slacts.NewSlackQuery(task.Query)
 
-				client := slacts.NewSlackClient(slackConfig.Token(), nil)
-				count, err := client.CountQuery(context.Background(), query)
-
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				log.Printf("name: %s, kind: %s, result: %d\n", task.Name, task.Kind, count)
-
-				if task.DoesSendDatadog() {
-					ddConfig := config.NewDatadogConfig()
-
-					if ddConfig.AppKey() == "" || ddConfig.APIKey() == "" {
-						log.Fatalln("unset Datadog API credentials. Need both API key and application key")
+					if slackConfig.Token() == "" {
+						log.Fatalln("unset Slack API Token")
 					}
 
-					ddClient := report.NewDatadogClient(ddConfig.APIKey(), ddConfig.AppKey(), nil)
+					client := slacts.NewSlackClient(slackConfig.Token(), nil)
+					count, err := client.CountQuery(context.Background(), query)
 
-					// TODO: add lacked properties. for example, Unit, Host and etc...
-					metrics := []report.DatadogMetric{
-						{
-							Metric: &task.Datadog.Metric,
-							Points: []report.DatadogDataPoint{
-								{
-									float64Pointer(float64(time.Now().Unix())),
-									float64Pointer(float64(count)),
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					log.Printf("name: %s, kind: %s, result: %d\n", task.Name, task.Kind, count)
+
+					if task.DoesSendDatadog() {
+						ddConfig := config.NewDatadogConfig()
+
+						if ddConfig.AppKey() == "" || ddConfig.APIKey() == "" {
+							log.Fatalln("unset Datadog API credentials. Need both API key and application key")
+						}
+
+						ddClient := report.NewDatadogClient(ddConfig.APIKey(), ddConfig.AppKey(), nil)
+
+						// TODO: add lacked properties. for example, Unit, Host and etc...
+						metrics := []report.DatadogMetric{
+							{
+								Metric: &task.Datadog.Metric,
+								Points: []report.DatadogDataPoint{
+									{
+										float64Pointer(float64(time.Now().Unix())),
+										float64Pointer(float64(count)),
+									},
 								},
+								Tags: task.Datadog.Tags,
 							},
-							Tags: task.Datadog.Tags,
-						},
-					}
+						}
 
-					if err := ddClient.PostMetrics(metrics); err != nil {
-						log.Fatal(err)
+						if err := ddClient.PostMetrics(metrics); err != nil {
+							log.Fatal(err)
+						}
+						log.Println("send metric to datadog successfully")
 					}
-					log.Println("send metric to datadog successfully")
 				}
 			}
-		}
-	},
+		},
+	}
+
+	cmd.PersistentFlags().StringVarP(&opt.file, "file", "f", "", "task definition file path")
+
+	return cmd
 }
 
-var slackCmd = &cobra.Command{
-	Use:   "slack",
-	Short: "commands for Slack",
+// newSlackCmd
+func newSlackCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "slack",
+		Short: "commands for Slack",
+	}
+
+	cmd.AddCommand(newSlackCountCmd())
+
+	return cmd
 }
 
-var slackCountCmd = &cobra.Command{
-	Use:     "count [query]",
-	Short:   "count messages what matches query",
-	Args:    cobra.ExactArgs(1),
-	Example: "slacts slack count \"in:#general message\"",
-	Run: func(cmd *cobra.Command, args []string) {
-		query := slacts.NewSlackQuery(args[0])
+func newSlackCountCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "count [query]",
+		Short:   "count messages what matches query",
+		Args:    cobra.ExactArgs(1),
+		Example: "slacts slack count \"in:#general message\"",
+		Run: func(cmd *cobra.Command, args []string) {
+			query := slacts.NewSlackQuery(args[0])
 
-		if slackConfig.Token() == "" {
-			log.Fatalln("unset Slack API Token")
-		}
+			if slackConfig.Token() == "" {
+				log.Fatalln("unset Slack API Token")
+			}
 
-		client := slacts.NewSlackClient(slackConfig.Token(), nil)
-		count, err := client.CountQuery(context.Background(), query)
+			client := slacts.NewSlackClient(slackConfig.Token(), nil)
+			count, err := client.CountQuery(context.Background(), query)
 
-		if err != nil {
-			log.Fatalln(err)
-		}
+			if err != nil {
+				log.Fatalln(err)
+			}
 
-		fmt.Printf("result: %d\n", count)
-	},
+			fmt.Printf("result: %d\n", count)
+		},
+	}
 }
 
 func float64Pointer(v float64) *float64 {
