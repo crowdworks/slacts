8f8448b16b012cc30030218219d5ce2b3da2c031
Shota SAWADA <xiootas@gmail.com>
2019-01-17 15:37:08 +0900

    manage CLI tools in Go code

---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+// Package tools manage CLI tools
+package tools
+
+import (
+	_ "github.com/jstemmer/go-junit-report"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+)
